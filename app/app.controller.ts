4a2ddb239947c36736fdc512cdeb24e0d903f217
Abhiseck Bhattacharya <abhiseck@outlook.com>
2026-02-08 17:24:03 +0530

    Revert "feat: integrate OpenTelemetry for distributed tracing, metrics, and logs."
    This reverts commit 1d7bb34e61c56445865deca9553976e74e249945.

--- app/app.controller.ts
+++ app/app.controller.ts
@@ -1,20 +1,8 @@
-import { SpanStatusCode, trace } from '@opentelemetry/api';
 import { Request, Response } from 'express';
 
-const tracer = trace.getTracer(
-  'express-typescript-backend-template-controller',
-  process.env.NODE_ENV === 'development' ? '1.0.0-dev' : '1.0.0',
-);
-
 export const rootController = async (req: Request, res: Response) => {
-  const span = tracer.startSpan('rootController');
-  span.setStatus({ code: SpanStatusCode.OK });
-  span.end();
   res.send('Hello World');
 };
 export const healthCheckController = async (req: Request, res: Response) => {
-  const span = tracer.startSpan('healthCheckController');
-  span.setStatus({ code: SpanStatusCode.OK });
-  span.end();
   res.sendStatus(200);
 };
