8fc78d1e877b7362fb6f9deb10f7d91ac78bfba6
Abhiseck Bhattacharya <abhiseck@outlook.com>
2026-07-10 17:54:08 +0530

    refactor: remove unused chat, ipLookup, and copyMe modules from codebase

--- app/app.route.ts
+++ app/app.route.ts
@@ -4,31 +4,24 @@ import { jwtAuth } from '../middlewares/jwtAuth.middlewares';
 import authRouter from './auth/auth.route';
 import blogRouter from './blog/blog.route';
 import changeLogRouter from './changeLog/changeLog.route';
-import copyMeRouter from './copyMe/copyMe.route';
-import ipLookupRouter from './ipLookup/ipLookup.route';
 import productRouter from './product/product.route';
 import testingRouter from './testing/testing';
-import chatRouter from './chat/chat.route';
 
 import {
-  sseDemoController,
-  readableStreamDemoController,
   longPollDemoController,
+  readableStreamDemoController,
+  sseDemoController,
 } from './app.controller';
 
 const appRouter = express.Router();
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/change-log', changeLogRouter);
-appRouter.use('/copy-me', jwtAuth, copyMeRouter);
 appRouter.use('/product', jwtAuth, productRouter);
 appRouter.use('/blog', blogRouter);
-appRouter.use('/ip', ipLookupRouter);
-appRouter.use('/chat', jwtAuth, chatRouter);
 appRouter.get('/sse-demo', sseDemoController);
 appRouter.get('/readable-stream-demo', readableStreamDemoController);
 appRouter.get('/long-poll-demo', longPollDemoController);
-
 
 if (process.env.NODE_ENV === 'development') {
   appRouter.use('/testing', testingRouter);
